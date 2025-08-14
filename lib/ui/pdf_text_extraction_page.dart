ec754a3fbe96c235def70807e7bc59a70da5db41
xrole <xrolediamond@gmail.com>
2025-08-14 16:45:45 +0100

    implement pdf picking and text extraction

--- lib/ui/pdf_text_extraction_page.dart
+++ lib/ui/pdf_text_extraction_page.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:ml_flutter/constants/app_colors.dart';
 import 'package:ml_flutter/ui/widget/action_button.dart';
 import 'package:ml_flutter/ui/widget/padded_container.dart';
+import 'package:pdf_text/pdf_text.dart';
 
 class PdfTextExtractionPage extends StatefulWidget {
   const PdfTextExtractionPage({super.key});
@@ -11,6 +15,33 @@ class PdfTextExtractionPage extends StatefulWidget {
 }
 
 class _PdfTextExtractionPageState extends State<PdfTextExtractionPage> {
+  File? pickedFile;
+  String? fileName;
+  String doc = "";
+
+  Future<void> pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result != null) {
+      File? file = File(result.files.single.path!);
+      PDFDoc pdfDoc = await PDFDoc.fromFile(file);
+      String extractedText = await pdfDoc.text;
+      print("extractedText =====================> $extractedText");
+      setState(() {
+        fileName = result.files.single.name;
+        pickedFile = file;
+        doc = extractedText;
+      });
+    } else {
+      setState(() {
+        fileName = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -24,6 +55,7 @@ class _PdfTextExtractionPageState extends State<PdfTextExtractionPage> {
                 SizedBox(height: size.height * .1),
                 Container(
                   height: size.height * .14,
+                  width: size.width,
                   decoration: BoxDecoration(
                     color: Colors.cyan[800],
                     borderRadius: BorderRadius.circular(8),
@@ -31,41 +63,67 @@ class _PdfTextExtractionPageState extends State<PdfTextExtractionPage> {
                   child: Column(
                     children: [
                       SizedBox(height: size.height * .03),
-                      ListTile(
-                        leading: Container(
-                          height: size.height * .06,
-                          width: size.height * .06,
-                          decoration: BoxDecoration(
-                            color: Colors.amber[700],
-                            borderRadius: BorderRadius.circular(
-                              size.height * .03,
-                            ),
-                          ),
-                          child: Center(
+                      pickedFile == null
+                          ? Center(
                             child: Text(
-                              "B",
+                              "No file loaded",
                               style: TextStyle(
-                                fontSize: size.height * 0.024,
-                                fontWeight: FontWeight.w800,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.height * .019,
+                              ),
+                            ),
+                          )
+                          : ListTile(
+                            leading: Container(
+                              height: size.height * .06,
+                              width: size.height * .06,
+                              decoration: BoxDecoration(
+                                color: Colors.amber[700],
+                                borderRadius: BorderRadius.circular(
+                                  size.height * .03,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "B",
+                                  style: TextStyle(
+                                    fontSize: size.height * 0.024,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              "Bank statement",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            subtitle: Text(
+                              fileName ?? "",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            trailing: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.visibility,
+                                color: Colors.grey[300],
                               ),
                             ),
                           ),
-                        ),
-                        title: Text(
-                          "Bank statement",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        subtitle: Text(
-                          "2023 Bank statement for pdf lovers",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        trailing: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.visibility, color: Colors.grey[300]),
-                        ),
-                      ),
                     ],
                   ),
+                ),
+                SizedBox(height: size.height * .015),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size.width * .04,
+                    vertical: size.height * .02,
+                  ),
+                  height: size.height * .5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: SingleChildScrollView(child: Text(doc)),
                 ),
               ],
             ),
@@ -73,11 +131,20 @@ class _PdfTextExtractionPageState extends State<PdfTextExtractionPage> {
             Column(
               children: [
                 SizedBox(height: size.height * .03),
-Center(child: Container(
-  decoration: BoxDecoration(
-    border: Border.all(color: AppColors.richBlue),borderRadius: BorderRadius.circular(size.height * .05)
-  ),
-  child: IconButton(onPressed: (){}, icon: Icon(Icons.add, size: size.height * .06,))),),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.richBlue),
+                      borderRadius: BorderRadius.circular(size.height * .05),
+                    ),
+                    child: IconButton(
+                      onPressed: () async {
+                        await pickFile();
+                      },
+                      icon: Icon(Icons.add, size: size.height * .06),
+                    ),
+                  ),
+                ),
                 SizedBox(height: size.height * .06),
               ],
             ),
